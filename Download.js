ebbb84e7dd51e4de81b7139d318f6628b1a8cbc4
virtual OS king <143428221+Martinka031@users.noreply.github.com>
2025-03-08 16:53:48 +0100

    Delete Download.js

--- Download.js
+++
@@ -1,4 +0,0 @@
-// Function to Download Repo ZIP
-        function downloadRepo() {
-            window.location.href = "https://github.com/Warp-Core-PW/Impact/raw/refs/heads/main/extensions.zip";
-        }
